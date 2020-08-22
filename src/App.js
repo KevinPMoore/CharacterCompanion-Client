37cad10eaac2be8030fa17d666410e8bd5307a9e
Kevin Moore <moore.kevin.p@gmail.com>
2020-08-22 11:57:59 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  state = {
+
+  };
+
+  render() {
+    return(
+      <h1>
+        Hello world
+      </h1>
+    );
+  };
+};
